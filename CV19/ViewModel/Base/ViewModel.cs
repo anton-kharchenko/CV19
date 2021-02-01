0caf211611cb0ae428e082134d708177e9b600a0
Gigabyte <gumb1t97@gmail.com>
2021-02-01 20:15:31 +0200

    Базовый класс модели-представления

---
+++ CV19/ViewModel/Base/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CV19.Annotations;
+
+namespace CV19.ViewModel.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (field.Equals(value)) return false;
+            
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+
+        }
+    }
+}
