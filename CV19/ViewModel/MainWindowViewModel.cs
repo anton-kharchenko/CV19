703957a0cc8e4ae3f3294fb9d9f53a8af0b806de
Gigabyte <gumb1t97@gmail.com>
2021-02-02 15:17:17 +0200

    Тестирование работы модели-представления

--- CV19/ViewModel/MainWindowViewModel.cs
+++ CV19/ViewModel/MainWindowViewModel.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModel
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : Base.ViewModel
     {
+        #region Заголовок окна
+
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
