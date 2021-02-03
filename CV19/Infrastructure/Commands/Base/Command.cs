168ea68db41ebdfe3b9fad665e4f097ad5280862
Gigabyte <gumb1t97@gmail.com>
2021-02-03 21:39:55 +0200

    Базовый класс комынды

---
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        /// <summary>
+        /// Говорит можно ли выполнить команду
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public abstract bool CanExecute(object parameter);
+
+        /// <summary>
+        /// То что должна делать команда
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
