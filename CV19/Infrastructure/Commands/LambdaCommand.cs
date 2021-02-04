1ef0d52849250698ba998992aef789748c002ea6
Gigabyte <gumb1t97@gmail.com>
2021-02-04 10:47:15 +0200

    Добавляем в модель-представления Главного окна команду выхода из приложения

--- CV19/Infrastructure/Commands/LambdaCommand.cs
+++ CV19/Infrastructure/Commands/LambdaCommand.cs
@@ -8,7 +8,7 @@ namespace CV19.Infrastructure.Commands
         private readonly Action<object> _Execute;
         private readonly Func<object, bool> _CanExecute;
 
-        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute =  null)
+        public LambdaCommand(Action<object> Execute,  Func<object, bool> CanExecute =  null)
         {
             _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
             _CanExecute = CanExecute;
--- CV19/ViewModel/MainWindowViewModel.cs
+++ CV19/ViewModel/MainWindowViewModel.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CV19.Infrastructure.Commands;
 
 namespace CV19.ViewModel
 {
     internal class MainWindowViewModel : Base.ViewModel
     {
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseAppApplicationCommand = new LambdaCommand(OnCloseAppApplicationCommandExecuted, CanCloseAppApplicationCommandExecute);
+
+            #endregion
+        }
+
+        #region Команды
+
+        #region Команда закрытия преложения
+
+        public ICommand CloseAppApplicationCommand { get;  }
+
+        void OnCloseAppApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        bool CanCloseAppApplicationCommandExecute(object p) => true;
+
+        #endregion
+
+        #endregion
+
         #region Заголовок окна
 
         private string _title = "Анализ статистики CV19";
@@ -21,7 +50,7 @@ namespace CV19.ViewModel
 
         #endregion
 
-        #region Status: string - Статус программы 
+        #region Status: string - Статус программы
 
         private string _status = "Готов!";
 
