354c675f8a96cd45bfd8cbe333f4db751d10c289
Gigabyte <gumb1t97@gmail.com>
2021-02-05 14:48:04 +0200

    Запрос к удаленному серверу для получения сырых данных

--- CV19Console/Program.cs
+++ CV19Console/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CV19Console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const string data =
+            @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        private static void Main(string[] args)
         {
+            // WebClient client = new WebClient();
+            var webClient = new HttpClient();
+            var response = webClient.GetAsync(data).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
