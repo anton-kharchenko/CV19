f5034a1b02c91e5e21f7ca5e6700b390f2bcd237
Gigabyte <gumb1t97@gmail.com>
2021-02-06 17:59:07 +0200

    Пример с управлением вкладок через модель представления

--- CV19/ViewModel/MainWindowViewModel.cs
+++ CV19/ViewModel/MainWindowViewModel.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using CV19.Infrastructure.Commands;
 using System.Windows;
 using System.Windows.Input;
+using CV19.Infrastructure.Commands;
 using CV19.Models;
 
 namespace CV19.ViewModel
 {
     internal class MainWindowViewModel : Base.ViewModel
     {
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseAppApplicationCommand = new LambdaCommand(OnCloseAppApplicationCommandExecuted,
+                CanCloseAppApplicationCommandExecute);
+
+            ChangeTabItem = new LambdaCommand(OnChangeTabItemExecuted, CanChangeTabItemExecuted);
+
+            #endregion Команды
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x += 0.1)
+            {
+                const double radius = Math.PI / 180;
+                var y = Math.Sin(radius * x);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            _TestPoints = data_points;
+        }
+
         #region IEnumerable<DataPoint> - Тестовый набор данных для визуализации интерфейса
 
         private IEnumerable<DataPoint> _TestPoints;
@@ -21,26 +44,6 @@ namespace CV19.ViewModel
 
         #endregion IEnumerable<DataPoint> - Тестовый набор данных для визуализации интерфейса
 
-        public MainWindowViewModel()
-        {
-            #region Команды
-
-            CloseAppApplicationCommand = new LambdaCommand(OnCloseAppApplicationCommandExecuted, CanCloseAppApplicationCommandExecute);
-
-            #endregion Команды
-
-            var data_points = new List<DataPoint>((int)(360 / 0.1));
-            for (double x = 0d; x <= 360; x += 0.1)
-            {
-                const double radius = Math.PI / 180;
-                var y = Math.Sin(radius * x);
-
-                data_points.Add(new DataPoint { XValue = x, YValue = y });
-            }
-
-            _TestPoints = data_points;
-        }
-
         #region Команды
 
         #region Команда закрытия преложения
@@ -52,9 +55,26 @@ namespace CV19.ViewModel
             Application.Current.Shutdown();
         }
 
-        private bool CanCloseAppApplicationCommandExecute(object p) => true;
+        private bool CanCloseAppApplicationCommandExecute(object p)
+        {
+            return true;
+        }
 
         #endregion Команда закрытия преложения
+
+        #region Переключатель вкладок
+
+        public ICommand ChangeTabItem { get; }
+
+        private void OnChangeTabItemExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedTabPage += Convert.ToInt32(p);
+        }
+
+        private bool CanChangeTabItemExecuted(object p) => SelectedTabPage >= 0;
+
+        #endregion Переключатель вкладок
 
         #endregion Команды
 
@@ -76,8 +96,26 @@ namespace CV19.ViewModel
         private string _status = "Готов!";
 
         /// <summary>Статус программы </summary>
-        public string Status { get => _status; set => Set(ref _status, value); }
+        public string Status
+        {
+            get => _status;
+            set => Set(ref _status, value);
+        }
 
         #endregion Status: string - Статус программы
+
+        #region SelectedTabPage
+
+        /// <summary>Номер выбраной вкладки</summary>
+        private int _selectedTabPage;
+
+        /// <summary>Номер выбраной вкладки</summary>
+        public int SelectedTabPage
+        {
+            get => _selectedTabPage;
+            set => Set(ref _selectedTabPage, value);
+        }
+
+        #endregion SelectedTabPage
     }
 }
