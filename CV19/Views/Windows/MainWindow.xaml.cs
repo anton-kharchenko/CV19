d48591313b6a6c171e5eb8c27b731ff553776f39
Gigabyte <gumb1t97@gmail.com>
2021-02-28 20:13:27 +0200

    Начал реализовывать событие фильтрации текста

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace CV19
+﻿using System.Linq;
+using System.Windows.Data;
+using CV19.Models.Decanat;
+
+namespace CV19
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -6,5 +10,20 @@
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
+
+        private void GroupCollectionFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name == null) return;
+
+            var filter_text = GroupNameFilterText.Text;
+
+            if (filter_text.Length == 0) return;
+
+            if (group.Name.Contains(filter_text)) return;
+            if (group.Description.Contains(filter_text)) return;
+
+            e.Accepted = false;
+        }
     }
 }
