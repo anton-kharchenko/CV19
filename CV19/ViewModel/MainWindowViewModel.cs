2f153d448a59d52339c19fa2260d3c173d741861
Gigabyte <gumb1t97@gmail.com>
2021-03-03 21:04:53 +0200

    Сортировка и группировка элементов коллекции

--- CV19/ViewModel/MainWindowViewModel.cs
+++ CV19/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 using CV19.Infrastructure.Commands;
-using CV19.Models;
 using CV19.Models.Decanat;
+using OxyPlot;
+using DataPoint = CV19.Models.DataPoint;
 
 namespace CV19.ViewModel
 {
@@ -45,9 +48,9 @@ namespace CV19.ViewModel
 
             var students = Enumerable.Range(1, 10).Select(i => new Student
             {
-                Name = $"Имя {student_index}",
-                Surname = $"Фамилия {student_index}",
-                Patronymic = $"Отчество {student_index}",
+                Name = $"Имя {i}",
+                Surname = $"Фамилия {i}",
+                Patronymic = $"Отчество {i}",
                 Birthday = DateTime.Now,
                 Rating = rating++
             });
@@ -69,6 +72,10 @@ namespace CV19.ViewModel
             data_list.Add(firstGroup.Students[0]);
 
             CompositeCollection = data_list.ToArray();
+
+            _SelectedGroupStudents.Filter += OnStudentFilter;
+            //_SelectedGroupStudents.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Descending));
+            //_SelectedGroupStudents.GroupDescriptions.Add(new PropertyGroupDescription("Name"));
         }
 
         #region IEnumerable<DataPoint> - Тестовый набор данных для визуализации интерфейса
@@ -205,6 +212,23 @@ namespace CV19.ViewModel
 
         #endregion SelectedTabPage: int - Выбраная Вкладка
 
+        #region Filter: string - Текст фильтра студентов
+
+        private string _StudentFilterText;
+
+        /// <summary>Текст фильтра студентов</summary>
+        public string StudentFilterText
+        {
+            get => _StudentFilterText;
+            set
+            {
+                if (!Set(ref _StudentFilterText, value)) return;
+                _SelectedGroupStudents.View.Refresh();
+            }
+        }
+
+        #endregion Filter: string - Текст фильтра студентов
+
         #region Selected Group: string -Выбраная группа
 
         private Group _selectedGroup;
@@ -213,10 +237,48 @@ namespace CV19.ViewModel
         public Group SelectedGroup
         {
             get => _selectedGroup;
-            set => Set(ref _selectedGroup, value);
+            set
+            {
+                if (!Set(ref _selectedGroup, value)) return;
+                _SelectedGroupStudents.Source = value?.Students;
+                OnPropertyChanged(nameof(SelectedGroupStudents));
+            }
+        }
+
+        private void OnStudentFilter(object sender, FilterEventArgs E)
+        {
+            if (!(E.Item is Student student))
+            {
+                E.Accepted = false;
+                return;
+            }
+
+            var filter_text = _StudentFilterText;
+            if (string.IsNullOrWhiteSpace(filter_text))
+                return;
+
+            if (student.Name is null || student.Surname is null)
+            {
+                E.Accepted = false;
+                return;
+            }
+
+            if (student.Name.Contains(filter_text)) return;
+            if (student.Surname.Contains(filter_text)) return;
+            if (student.Patronymic.Contains(filter_text)) return;
+
+            E.Accepted = false;
         }
 
         #endregion Selected Group: string -Выбраная группа
+
+        #region SelectedGroupStudents
+
+        public readonly CollectionViewSource _SelectedGroupStudents = new CollectionViewSource();
+
+        public ICollectionView SelectedGroupStudents => _SelectedGroupStudents?.View;
+
+        #endregion SelectedGroupStudents
 
         public IEnumerable<Student> TestStudents =>
             Enumerable.Range(1, App.IsDesignMode ? 10 : 100000)
