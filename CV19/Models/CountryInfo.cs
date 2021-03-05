72ab2d795f92a403255cd98654ad2a1f2b645d02
Gigabyte <gumb1t97@gmail.com>
2021-03-05 18:47:42 +0200

    Получение Covid-19 данных, новые Models объекты

--- CV19/Models/CountryInfo.cs
+++ CV19/Models/CountryInfo.cs
@@ -4,6 +4,6 @@ namespace CV19.Models
 {
     internal class CountryInfo : PlaceInfo
     {
-        public IEnumerable<ProvinceInfo> ProvincesCount { get; set; }
+        public IEnumerable<PlaceInfo> ProvincesCount { get; set; }
     }
 }
