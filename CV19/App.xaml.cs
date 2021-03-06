5574743b0fb56286529a7fd03332b77e451bd998
Gigabyte <gumb1t97@gmail.com>
2021-03-06 10:11:49 +0200

    Создание словаря ресурсов для GridSplitters

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -15,10 +15,6 @@ namespace CV19
         {
             IsDesignMode = false;
             base.OnStartup(e);
-
-            var service_test = new DataService();
-
-            var countries = service_test.GetData().ToArray();
         }
     }
 }
--- CV19/Models/CountryInfo.cs
+++ CV19/Models/CountryInfo.cs
@@ -1,9 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 
 namespace CV19.Models
 {
     internal class CountryInfo : PlaceInfo
     {
+        private Point? _Location;
+
+        public override Point Location
+        {
+            get
+            {
+                if (_Location != null)
+                    return (Point)_Location;
+                if (ProvincesCount is null) return default;
+
+                var average_x = ProvincesCount.Average(p => p.Location.X);
+                var average_y = ProvincesCount.Average(p => p.Location.Y);
+
+                return (Point)(_Location = new Point(average_x, average_y));
+            }
+            set => _Location = value;
+        }
+
         public IEnumerable<PlaceInfo> ProvincesCount { get; set; }
     }
 }
