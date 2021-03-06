1c0109c0dd0e8479e485d85038220c63d8ed5215
Gigabyte <gumb1t97@gmail.com>
2021-03-06 13:17:32 +0200

    Создание вторичной ViewModel для логики представления статистики

---
+++ CV19/Views/CountriesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CV19.Services;
+using CV19.ViewModel;
+
+namespace CV19.Views
+{
+    internal class CountriesStatisticViewModel : ViewModel.Base.ViewModel
+    {
+        private DataService DataService;
+        private MainWindowViewModel MainModel { get; }
+
+        public CountriesStatisticViewModel(MainWindowViewModel mainModel)
+        {
+            MainModel = mainModel;
+            DataService = new DataService();
+        }
+    }
+}
