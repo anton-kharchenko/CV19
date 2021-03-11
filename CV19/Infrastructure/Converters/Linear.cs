4ac2bc34ad2276d8d0242fa4e0edc6a212599e29
Gigabyte <gumb1t97@gmail.com>
2021-03-11 10:59:58 +0200

    Указание типов возвращаемых значений для дизайнера ValueConversion

--- CV19/Infrastructure/Converters/Linear.cs
+++ CV19/Infrastructure/Converters/Linear.cs
@@ -1,27 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace CV19.Infrastructure.Converters
 {
+    [ValueConversion(typeof(double), typeof(double))]
     /// <summary>Реализация линейного преобразования f(x) = k*x + b  </summary>
     internal class Linear : Convertor
     {
+        [ConstructorArgument("K")]
         public double K { get; set; } = 1;
+
+        [ConstructorArgument("B")]
         public double B { get; set; }
 
         public Linear()
         {
         }
 
-        public Linear(double k, double b)
+        public Linear(double k)
         {
             K = k;
-            B = b;
         }
+
+        public Linear(double k, double b) : this(k) => B = b;
 
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
