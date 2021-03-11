5685e9fc0aa4028f5fd96824fd29923f4d105237
Gigabyte <gumb1t97@gmail.com>
2021-03-11 17:55:44 +0200

    CompositeConverter

---
+++ CV19/Infrastructure/Converters/CompositeConvertor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class CompositeConvertor : Convertor
+    {
+        public IValueConverter First { get; set; }
+        public IValueConverter Second { get; set; }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result1 = First?.Convert(value, targetType, parameter, culture) ?? value;
+            var result2 = First?.Convert(result1, targetType, parameter, culture) ?? result1;
+
+            return result2;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result2 = First?.Convert(value, targetType, parameter, culture) ?? value;
+            var result1 = First?.Convert(result2, targetType, parameter, culture) ?? result2;
+
+            return result1;
+        }
+    }
+}
