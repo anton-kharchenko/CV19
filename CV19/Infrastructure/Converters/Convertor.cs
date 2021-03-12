56e13cad784e7e06181f20bc168a9af875cd9635
Gigabyte <gumb1t97@gmail.com>
2021-03-12 11:16:15 +0200

    MultiConverter

--- CV19/Infrastructure/Converters/Convertor.cs
+++ CV19/Infrastructure/Converters/Convertor.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace CV19.Infrastructure.Converters
@@ -12,7 +8,9 @@ namespace CV19.Infrastructure.Converters
     {
         public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
 
-        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
             throw new NotSupportedException("Обратное преобразование не поддерживается");
+        }
     }
 }
---
+++ CV19/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class ToArray : MultiConverter
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collection = new CompositeCollection();
+            foreach (var item in values)
+                collection.Add(item);
+
+            return collection;
+        }
+
+        // public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => values as object;
+    }
+}
