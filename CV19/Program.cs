82344a2ec206f226c6108a3aa6e045847af8759e
Gigabyte <gumb1t97@gmail.com>
2021-03-15 21:49:46 +0200

    Инфраструктура для создания хоста приложения

--- CV19/Program.cs
+++ CV19/Program.cs
@@ -1,7 +1,37 @@
-﻿namespace CV19
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace CV19
 {
-    public class Program
+    public static class Program
     {
-        
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(Args);
+
+            // Указываем текущую папку
+            hostBuilder.UseContentRoot(Environment.CurrentDirectory);
+            // Добавляем кофигурацию приложения
+            hostBuilder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                // Где будет создан файл кофигурации
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                // Какой файл нужно создать
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            hostBuilder.ConfigureServices(App.ConfigureServices());
+
+            return hostBuilder;
+        }
     }
 }
