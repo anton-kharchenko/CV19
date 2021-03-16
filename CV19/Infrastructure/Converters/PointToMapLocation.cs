99743c11952f75589900aaa2662fc8e9502bf665
Gigabyte <gumb1t97@gmail.com>
2021-03-16 11:09:09 +0200

    Почистил код

--- CV19/Infrastructure/Converters/PointToMapLocation.cs
+++ CV19/Infrastructure/Converters/PointToMapLocation.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using MapControl;
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
-using MapControl;
 
 namespace CV19.Infrastructure.Converters
 {
--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System.Linq;
+﻿using CV19.Models.Decanat;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Data;
-using CV19.Models.Decanat;
 
 namespace CV19
 {
