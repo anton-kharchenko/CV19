aff91af2d6a27bcbd68ef187c016daa5ee78c2ee
Gigabyte <gumb1t97@gmail.com>
2021-03-16 10:45:10 +0200

    Запуск и остановка хоста

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using CV19.Services;
 using CV19.ViewModel;
@@ -14,10 +15,25 @@ namespace CV19
     {
         public static bool IsDesignMode { get; private set; } = true;
 
-        protected override void OnStartup(StartupEventArgs e)
+        private static IHost _host;
+
+        public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
         {
             IsDesignMode = false;
+            var host = Host;
             base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _host = null;
         }
 
         public static void ConfigureServices(IHostBuilder hostBuilder, IServiceCollection serviceCollection)
