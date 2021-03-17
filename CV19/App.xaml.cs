773d41747be961cb1e76b036d366b970a0efc465
Gigabyte <gumb1t97@gmail.com>
2021-03-17 12:03:45 +0200

    Добавлен точка входа Program.cs

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,20 +1,43 @@
-﻿using System.Linq;
-using System.Windows;
+﻿using System;
 using CV19.Services;
+using CV19.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace CV19
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
         public static bool IsDesignMode { get; private set; } = true;
+        private static IHost _host { get; set; }
 
-        protected override void OnStartup(StartupEventArgs e)
+        public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
         {
             IsDesignMode = false;
             base.OnStartup(e);
+
+            var host = Host;
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            _host = null;
+        }
+
+        public static void ConfigureServices(HostBuilderContext hostBuilderContext, IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<DataService>();
+            serviceCollection.AddSingleton<CountriesStatisticViewModel>();
         }
     }
 }
---
+++ CV19/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using FileConfigurationExtensions = Microsoft.Extensions.Configuration.FileConfigurationExtensions;
+
+namespace CV19
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        public static void Main()
+        {
+            var app = new App();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(Args);
+
+            hostBuilder.UseContentRoot(Environment.CurrentDirectory);
+            hostBuilder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+            hostBuilder.ConfigureServices(App.ConfigureServices);
+
+            return hostBuilder;
+        }
+    }
+}
