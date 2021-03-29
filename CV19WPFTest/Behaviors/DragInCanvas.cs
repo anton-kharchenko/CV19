0a17e0ce142d9ae86609a95333a33809e0a208b6
Gigabyte <gumb1t97@gmail.com>
2021-03-29 21:03:20 +0200

    Поведение, способное реагировать на событие мыши

---
+++ CV19WPFTest/Behaviors/DragInCanvas.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19WPFTest.Behaviors
+{
+    internal class DragInCanvas : Behavior<UIElement>
+    {
+        private Point _startPoint;
+        private Canvas _canvas;
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += OnButtonDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseLeftButtonDown -= OnButtonDown;
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.MouseUp -= OnMouseUp;
+        }
+
+        private void OnButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if ((_canvas ??= VisualTreeHelper.GetParent(AssociatedObject) as Canvas) is null)
+                return;
+
+            _startPoint = e.GetPosition(AssociatedObject);
+            AssociatedObject.CaptureMouse();
+            AssociatedObject.MouseMove += OnMouseMove;
+            AssociatedObject.MouseUp += OnMouseUp;
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.MouseUp -= OnMouseUp;
+            AssociatedObject.ReleaseMouseCapture();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var obj = AssociatedObject;
+            var currentPos = e.GetPosition(obj);
+
+            var delta = currentPos - _startPoint;
+
+            obj.SetValue(Canvas.LeftProperty, delta.X);
+            obj.SetValue(Canvas.TopProperty, delta.Y);
+        }
+    }
+}
